968ca0aab93bfdef22259cde29d6d77812c244fa
knottykoder <hemant.kumar472@gmail.com>
2024-08-12 19:22:55 +0530

    add meida upload and integrate cloudinary

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -28,6 +28,7 @@ import {
 } from "@/constants";
 import { CustomField } from "./CustomField";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -112,13 +113,11 @@ const TransformationForm = ({
     setTransformationConfig(
       deepMergeObjects(newTransformation, transformationConfig)
     );
-    setNewTransformation(null)
+    setNewTransformation(null);
 
-    startTransition(async ()=>{
-
-    })
+    startTransition(async () => {});
   };
-  
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -206,6 +205,23 @@ const TransformationForm = ({
           )}
         </div>
       )}
+
+      <div className="media-uploader-field">
+        <CustomField
+          control={form.control}
+          name="publicId"
+          className="flex size-full flex-col"
+          render={({ field }) => (
+            <MediaUploader 
+            onValueChange={field.onChange}
+            setImage={setImage}
+            publicId={field.value}
+            image={image}
+            type={type}
+          />
+          )}
+        />
+      </div>
       <div className="flex flex-col gap-4">
         <Button
           className="submit-button capitalize"
